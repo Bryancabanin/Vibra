0a4ca4b02491291984147303ec77530a8439cbf1
Richard Rivera <richard.rivera.roman@gmail.com>
2025-03-04 16:11:27 -0500

    Fix: enviroment variables

--- server/supabaseClient.ts
+++ server/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Get environment variables
 const supabaseUrl = 'https://rygvbhetquwltojwczhr.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY!;
 
 // Create the Supabase client
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
